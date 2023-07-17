506dd83668c7685331ac30aa5c06b12c2dc34605
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-07-16 23:29:38 -0500

    try

---
+++ src/py/SQLAlchemy/Demos Petsas/models/user.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import Relationship
+
+from models.base import TimeStampedModel
+
+
+class User(TimeStampedModel):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(80), nullable=False)
+    last_name = Column(String(80), nullable=False)
+    email = Column(String(320), nullable=False, unqiue=True)
+
+    preference = Relationship("Preference", back_populates="user", uselist=False, passive_deletes=True)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}, name: {self.first_name} {self.last_name}"
+
+class Preference(TimeStampedModel):
+    __tablename__ "preferences"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    language = Column(String(80), nullable=False)
+    currency = Column(String(3), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True, unique=True)
+
+    user = Relationship("User", back_populates="preference")

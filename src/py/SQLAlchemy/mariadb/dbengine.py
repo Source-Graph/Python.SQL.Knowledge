45f07eb9809ff3ecaa378ceedb7f0a1bb4d30350
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-07-18 05:28:10 -0500

    dbengine.py

---
+++ src/py/SQLAlchemy/mariadb/dbengine.py
@@ -0,0 +1,51 @@
+from sqlalchemy import create_engine, text
+
+
+class DBengine:
+    def __init__(self, user="user", passwd="password", host="localhost", name="testdatabase"):
+        self.user = user
+        self.passwd = passwd
+        self.host = host
+        self.name = name
+        self.master = create_engine(f"mysql+pymysql://{self.user}:{self.passwd}@{self.host}")
+
+    def query_exp(self, expression):
+        with self.master.connect() as conn:
+            result = conn.execute(expression)
+            return result.all()
+            conn.commit()
+
+    def query(self, expression):
+        with self.master.connect() as conn:
+            conn.execute(expression)
+
+    def list_database(self):
+        return self.query_exp(text("SHOW DATABASES"))
+
+    def exists(self, name):
+        all = self.list_database()
+        dbs = set()
+        for i in all:
+            dbs.add(i[0])
+        return name in dbs
+
+    def create_database(self):
+        if db.exists(self.name):
+            print(f"{self.name}, already exists!")
+        else:
+            self.query(text(f"CREATE DATABASE {self.name}"))
+            print(f"Created database {self.name}")
+
+    def delete_database(self):
+        if db.exists(self.name):
+            self.query(text(f"DROP DATABASE {self.name}"))
+            print(f"Deleted database {self.name}")
+        else:
+            print(self.list_database())
+
+
+if __name__ == '__main__':
+    db = DBengine()
+    db.create_database()
+    db.delete_database()
+
